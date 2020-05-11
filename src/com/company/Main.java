356b5b385edecb7cc351079d398ccc8de61ac36e
sajanav <vsajana@gmail.com>
2020-05-11 11:00:32 +0400

    withexplicitwaitandXpath
    withexplicitwaitandXpath

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,32 @@
 package com.company;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 public class Main {
 
     public static void main(String[] args) {
     	System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
 
     	 WebDriver driver = new ChromeDriver();
+    	
 
         String baseUrl = "https://www.calogi.com";
         String expectedTitle = "Airfreight Specialists | Calogi";
         String actualTitle = "";
 
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("/html/head/title")));
+
+
         // launch Fire fox and direct it to the Base URL
+
         driver.get(baseUrl);
 
         // get the actual value of the title
@@ -23,7 +36,7 @@ public class Main {
       /*   * compare the actual title of the page with the expected one and print
          * the result as "Passed" or "Failed"
        */  
-        if (actualTitle.contentEquals(expectedTitle)){
+        if (actualTitle.equals(expectedTitle)){
             System.out.println("Test Passed!");
         } else {
             System.out.println("Test Failed");
